9a29d5197431af065c7e61768e8ff98f73e799a8
daniw <dani.winz@bluewin.ch>
2016-05-09 23:08:44 +0200

    First draft of data collecting script

---
+++ park.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while :
+do
+    filename=`date +"park_%y%m%d.log"`
+    wget http://hslu.ximit.ch/ > /dev/null 2>&1
+    cat index.html | grep "Freie Parkpl" | sed 's|<[^>]*>||g' | sed 's| HSLU T&amp;A Horw:Freie Parkpl&auml;tze: ||g' | sed 's|iPhone &amp; Co: Mobile App||g' >> $filename
+    rm index.html
+    tail -1 $filename
+    sleep 60
+done
