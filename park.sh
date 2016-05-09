d91ce71c8e3ddb8d208f56cf9be68bd8a77575a7
daniw <dani.winz@bluewin.ch>
2016-05-09 23:19:38 +0200

    Output prepared for matlab

--- park.sh
+++ park.sh
@@ -3,7 +3,7 @@ while :
 do
     filename=`date +"park_%y%m%d.log"`
     wget http://hslu.ximit.ch/ > /dev/null 2>&1
-    cat index.html | grep "Freie Parkpl" | sed 's|<[^>]*>||g' | sed 's| HSLU T&amp;A Horw:Freie Parkpl&auml;tze: ||g' | sed 's|iPhone &amp; Co: Mobile App||g' >> $filename
+    cat index.html | grep "Freie Parkpl" | sed 's|<[^>]*>||g' | sed 's| HSLU T&amp;A Horw:Freie Parkpl&auml;tze: ||g' | sed 's|iPhone &amp; Co: Mobile App||g' | sed 's|(|(60*|' | sed 's|:|+|' | sed 's|)|);|' >> $filename
     rm index.html
     tail -1 $filename
     sleep 60
