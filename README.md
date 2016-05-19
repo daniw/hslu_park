7018be925dc7f303b8ab5c5da0101794e757d201
daniw <dani.winz@bluewin.ch>
2016-05-19 05:05:46 +0200

    log2m plot improved

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hslu_park
 Script for parking lot statistics at HSLU
+
+## Requirements
+* wget
+
+## Usage
+./park.sh
+
+### Run in background
+./park.sh > /dev/null &
+
+## Convert log files into matlab scripts
+./log2m.sh
--- log2m.sh
+++ log2m.sh
@@ -6,5 +6,11 @@ do
     echo "p = [" > matlab/$i.m
     cat $i.log >> matlab/$i.m
     echo "];" >> matlab/$i.m
-    echo "plot(p(:,2)./60, p(:,1));" >> matlab/$i.m
+    echo "plot(p(:,2)./60, p(:,1), 'LineWidth', 2);" >> matlab/$i.m
+    echo "axis([0 24 0 400]);" >> matlab/$i.m
+    echo "xlabel('Daytime');" >> matlab/$i.m
+    echo "ylabel('Free parking spaces');" >> matlab/$i.m
+    echo "title('HSLU parking space');" >> matlab/$i.m
+    echo "grid on;" >> matlab/$i.m
+    echo "grid minor;" >> matlab/$i.m
 done;
